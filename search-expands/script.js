586b57a574047825dd95a47e2e794d3718fd0d4e
Talha Habib <talhahabib842653@gmail.com>
2026-07-29 16:12:53 +0500

    Add initial HTML, CSS, and JavaScript for search expand feature

---
+++ search-expands/script.js
@@ -0,0 +1,7 @@
+let input = document.querySelector('input');
+let button = document.querySelector('button');
+let holder = document.querySelector('.holder')
+button.addEventListener('click',()=>{
+  holder.classList.toggle('next');
+}
+)
