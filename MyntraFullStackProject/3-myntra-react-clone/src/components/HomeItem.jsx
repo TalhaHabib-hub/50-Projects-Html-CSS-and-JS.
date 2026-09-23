317f6240912f125efe7d29b3a7ee49d9515afd4d
Talha Habib <talhahabib842653@gmail.com>
2026-09-23 22:48:34 +0500

    start final fullstack project as a finishing task of  react and redux course

---
+++ MyntraFullStackProject/3-myntra-react-clone/src/components/HomeItem.jsx
@@ -0,0 +1,28 @@
+import itemImage from '../../public/images/7.jpg'
+
+const HomeItem = ({ item }) => {
+  return (
+    <>
+      <div className="item-container">
+        <img className="item-image" src={itemImage} alt="item image" />
+        <div className="rating">
+          {item.rating.stars} ⭐ | {item.rating.count}
+        </div>
+        <div className="company-name">{item.company}</div>
+        <div className="item-name">{item.item_name}</div>
+        <div className="price">
+          <span className="current-price">Rs {item.current_price}</span>
+          <span className="original-price">Rs {item.original_price}</span>
+          <span className="discount">({item.discount_percentage}% OFF)</span>
+        </div>
+        <button
+          className="btn-add-bag"
+          onclick={() => console.log("item has been clicked")}
+        >
+          Add to Bag
+        </button>
+      </div>
+    </>
+  );
+};
+export default HomeItem;
